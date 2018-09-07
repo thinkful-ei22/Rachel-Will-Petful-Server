d7c4f21a8c08da766fde03eeeef51e2b9edf4f4c
rpinto <rachel.lea.pinto@gmail.com>
2018-09-07 09:02:09 -0700

    moved endpoints out of sepret routes folder and into index, modified GET endpoints so they return index 0 of data arrays. Wrote DELETE endpoints

--- routes/cat.js
+++ routes/cat.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  const dummyCat = {
-    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
-    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-    name: 'Fluffy',
-    sex: 'Female',
-    age: 2,
-    breed: 'Bengal',
-    story: 'Thrown on the street'
-  };
+// router.get('/', (req, res, next) => {
+//   const dummyCat = {
+//     imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+//     imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+//     name: 'Fluffy',
+//     sex: 'Female',
+//     age: 2,
+//     breed: 'Bengal',
+//     story: 'Thrown on the street'
+//   };
 
-  res.json(dummyCat)
-});
+//   res.json(dummyCat)
+// });
 
-module.exports = router; 
+// module.exports = router; 
