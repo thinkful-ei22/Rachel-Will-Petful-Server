5b9e23ed55b7d6b3303666f302189382829df305
rpinto <rachel.lea.pinto@gmail.com>
2018-09-07 11:18:02 -0700

    added queue class, updated array of data so they are now instances

--- index.js
+++ index.js
@@ -1,55 +1,5 @@
 'use strict';
 
-const dogArray = [{
-  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-  imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
-  name: 'Zeus',
-  sex: 'Male',
-  age: 3,
-  breed: 'Golden Retriever',
-  story: 'Owner Passed away'
-}, {
-  imageURL: "https://hairstylecamp.com/wp-content/uploads/long-haired-duchshund.jpg",
-  imageDescription: 'A miniature long haired Daschund running joyfully in the grass',
-  name: "Fredrick",
-  sex: "male",
-  age: 12,
-  breed: "Miniature long haired Daschund",
-  story: "Former champion abandoned after he became too old to compete in the Doxie Derby"
-}, {
-  imageURL: "https://cdn2-www.dogtime.com/assets/uploads/gallery/saint-bernard-dogs-and-puppies/saint-bernard-dogs-puppies-1.jpg",
-  name: "Nana",
-  sex: "female",
-  age: 3,
-  breed: "St. Bernard",
-  story: "Owners retired and moved to a tropical climate after deciding they no longer wanted to or needed help to take care of their grandchildren"
-}]
-const catArray = [{
-  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
-  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-  name: 'Fluffy',
-  sex: 'Female',
-  age: 2,
-  breed: 'Bengal',
-  story: 'Thrown on the street'
-}, {
-  imageURL:'https://d3544la1u8djza.cloudfront.net/APHI/Blog/2016/12_December/what+do+Russian+Blue+cats+look+like+_+cat+resting+on+a+sofa.jpg', 
-  imageDescription: 'Russian blue cat looking at camera ',
-  name: 'Merideth',
-  sex: 'Female',
-  age: 7,
-  breed: 'Russian Blue',
-  story: 'Very sweet kitty, just has an unfortunate resting bitch face'
-}, {
-  imageURL:'https://talgv.org/wp-content/uploads/2017/01/Oolong_2-400x250.jpg', 
-  imageDescription: 'Diluted tortoiseshell kitten lounging',
-  name: 'Ella',
-  sex: 'Female',
-  age: 1,
-  breed: 'Diluted tortoiseshell',
-  story: 'Found in park just a few days old'
-} ]
-
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
@@ -58,12 +8,69 @@ const { PORT, CLIENT_ORIGIN } = require('./config');
 const { dbConnect } = require('./db-mongoose');
 // const {dbConnect} = require('./db-knex');
 
-// // ~~ GET routes ~~ //
-// const catRouter = require('./routes/cat');
-// const dogRouter = require('./routes/dog');
-
 const app = express();
+const Queue = require('./queueClass');
 
+//Queue instance of DOGS
+const dogsQ = new Queue();
+dogsQ.enqueue({
+  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+  imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+  name: 'Zeus',
+  sex: 'Male',
+  age: 3,
+  breed: 'Golden Retriever',
+  story: 'Owner Passed away'
+});
+dogsQ.enqueue({
+  imageURL: "https://hairstylecamp.com/wp-content/uploads/long-haired-duchshund.jpg",
+  imageDescription: 'A miniature long haired Daschund running joyfully in the grass',
+  name: "Fredrick",
+  sex: "male",
+  age: 12,
+  breed: "Miniature long haired Daschund",
+  story: "Former champion abandoned after he became too old to compete in the Doxie Derby"
+});
+dogsQ.enqueue({
+  imageURL: "https://cdn2-www.dogtime.com/assets/uploads/gallery/saint-bernard-dogs-and-puppies/saint-bernard-dogs-puppies-1.jpg",
+  name: "Nana",
+  sex: "female",
+  age: 3,
+  breed: "St. Bernard",
+  story: "Owners retired and moved to a tropical climate after deciding they no longer wanted to or needed help to take care of their grandchildren"
+});
+//console.log(dogsQ);
+
+//Queue instance of CATS
+const catsQ = new Queue();
+catsQ.enqueue({
+  imageURL: 'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+});
+catsQ.enqueue({
+  imageURL: 'https://d3544la1u8djza.cloudfront.net/APHI/Blog/2016/12_December/what+do+Russian+Blue+cats+look+like+_+cat+resting+on+a+sofa.jpg',
+  imageDescription: 'Russian blue cat looking at camera ',
+  name: 'Merideth',
+  sex: 'Female',
+  age: 7,
+  breed: 'Russian Blue',
+  story: 'Very sweet kitty, just has an unfortunate resting bitch face'
+});
+catsQ.enqueue({
+  imageURL: 'https://talgv.org/wp-content/uploads/2017/01/Oolong_2-400x250.jpg',
+  imageDescription: 'Diluted tortoiseshell kitten lounging',
+  name: 'Ella',
+  sex: 'Female',
+  age: 1,
+  breed: 'Diluted tortoiseshell',
+  story: 'Scrappy little runt of the litter found in park just a few days old'
+});
+//console.log(catsQ);
 
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
@@ -77,22 +84,29 @@ app.use(
   })
 );
 
-// GET first cat // 
-app.get('/api/cat', (req,res, next) => {
-  return res.json(catArray[0]);
+//peek() function
+function peek(q){
+  return q.first;
+}
+
+//---- GET first cat ----// 
+app.get('/api/cat', (req, res, next) => {
+  return res.json(peek(catsQ));
 })
 
-// DELETE  first cat //
+console.log(peek(catsQ))
+
+//---- DELETE  first cat ----//
 app.delete('/api/cat', (req, res) => {
   res.json(catArray.shift()).sendStatus(200);
 })
 
-// GET first dog //
+//---- GET first dog ----//
 app.get('/api/dog', (req, res, next) => {
   return res.json(dogArray[0]);
 })
 
-// DELETE first dog //
+//---- DELETE first dog ----//
 app.delete('/api/dog', (req, res) => {
   res.json(dogArray.shift()).sendStatus(200);
 });
